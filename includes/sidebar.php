41d26618ad4382eb3800e6b6237408980f6b6066
Nay Khant Kyaw <naykhantkyaw@acedatasystems.com>
2025-10-06 14:08:33 +0630

    fix UI and complete User Management

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -22,16 +22,7 @@
   </div>
 
   <div class="menu-inner-shadow"></div>
-  
-    <ul class="menu-inner py-1 backcolor">
-
-      <!-- User Management -->
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon icon-base ti tabler-smart-home textcolor"></i>
-          <div data-i18n="User Management" class="textcolor">User Management</div>
-        </a>
-        <!-- <ul class="menu-sub">
+    <!-- <ul class="menu-sub">
           <li class="menu-item active">
             <a href="index.html" class="menu-link">
               <div data-i18n="Analytics">Analytics</div>
@@ -58,132 +49,39 @@
             </a>
           </li>
         </ul> -->
+
+    <ul class="menu-inner py-1 backcolor">
+
+      <!-- User Management -->
+      <li class="menu-item">
+        <a href="usermanagement.php" class="menu-link">
+          <i class="menu-icon icon-base ti tabler-smart-home textcolor"></i>
+          <div data-i18n="User Management" class="textcolor">User Management</div>
+        </a>
       </li>
 
       <!-- Movie Management -->
       <li class="menu-item ">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <a href="javascript:void(0);" class="menu-link">
           <i class="menu-icon icon-base ti tabler-layout-sidebar textcolor"></i>
           <div data-i18n="Movie Management" class="textcolor">Movie Management</div>
         </a>
-
-        <!-- <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="layouts-collapsed-menu.html" class="menu-link">
-              <div data-i18n="Collapsed menu">Collapsed menu</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-content-navbar.html" class="menu-link">
-              <div data-i18n="Content navbar">Content navbar</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-content-navbar-with-sidebar.html" class="menu-link">
-              <div data-i18n="Content nav + Sidebar">Content nav + Sidebar</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../horizontal-menu-template/" class="menu-link" target="_blank">
-              <div data-i18n="Horizontal">Horizontal</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-without-menu.html" class="menu-link">
-              <div data-i18n="Without menu">Without menu</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-without-navbar.html" class="menu-link">
-              <div data-i18n="Without navbar">Without navbar</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-fluid.html" class="menu-link">
-              <div data-i18n="Fluid">Fluid</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-container.html" class="menu-link">
-              <div data-i18n="Container">Container</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-blank.html" class="menu-link">
-              <div data-i18n="Blank">Blank</div>
-            </a>
-          </li>
-        </ul> -->
       </li>
 
       <!-- Ticket Fees Management -->
       <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <a href="javascript:void(0);" class="menu-link">
           <i class="menu-icon icon-base ti tabler-files textcolor"></i>
           <div data-i18n="Ticket Fees Management" class="textcolor">Ticket Fees Management</div>
         </a>
-        <!-- <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="../front-pages/landing-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Landing">Landing</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/pricing-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Pricing">Pricing</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/payment-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Payment">Payment</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/checkout-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Checkout">Checkout</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/help-center-landing.html" class="menu-link" target="_blank">
-              <div data-i18n="Help Center">Help Center</div>
-            </a>
-          </li>
-        </ul> -->
       </li>
 
        <!-- Booking Management -->
       <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <a href="javascript:void(0);" class="menu-link">
           <i class="menu-icon icon-base ti tabler-files textcolor"></i>
           <div data-i18n="Booking Management" class="textcolor">Booking Management</div>
         </a>
-        <!-- <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="../front-pages/landing-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Landing">Landing</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/pricing-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Pricing">Pricing</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/payment-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Payment">Payment</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/checkout-page.html" class="menu-link" target="_blank">
-              <div data-i18n="Checkout">Checkout</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="../front-pages/help-center-landing.html" class="menu-link" target="_blank">
-              <div data-i18n="Help Center">Help Center</div>
-            </a>
-          </li>
-        </ul> -->
       </li>
 
       
