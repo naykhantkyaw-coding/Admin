e1a164adac12db07a4ca04d4be926d85a35eda3f
Nay Khant Kyaw <naykhantkyaw@acedatasystems.com>
2025-10-06 12:22:00 +0630

    -

--- index.php
+++ index.php
@@ -29,38 +29,6 @@ include('includes/header.php');
           </tr>
         </thead>
         <tbody class="table-border-bottom-0">
-          <!-- <tr>
-            <td>
-              <i class="icon-base ti tabler-brand-angular icon-md text-danger me-4"></i>
-              <span class="fw-medium">Angular Project</span>
-            </td>
-            <td>Albert Cook</td>
-            <td>
-              <ul class="list-unstyled m-0 avatar-group d-flex align-items-center">
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                  <img src="../../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
-                  <img src="../../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle" />
-                </li>
-                <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
-                  <img src="../../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle" />
-                </li>
-              </ul>
-            </td>
-            <td><span class="badge bg-label-primary me-1">Active</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                  <i class="icon-base ti tabler-dots-vertical"></i>
-                </button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="icon-base ti tabler-pencil me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="icon-base ti tabler-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr> -->
           <?php foreach ($users as $user): 
             $status_class = 'bg-label-secondary';
             $status_text = strtolower($user['Status'] ?? '');
